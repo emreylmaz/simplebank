0467cbfba40ebad95956e230bae2d9dbc9ac96f5
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2023-10-30 22:27:05 +0300

    Add unit tests

--- src/test/java/com/digitalemre/simplebank/services/impl/TransactionServiceImplTest.java
+++ src/test/java/com/digitalemre/simplebank/services/impl/TransactionServiceImplTest.java
@@ -1,13 +1,19 @@
 package com.digitalemre.simplebank.services.impl;
 
 import com.digitalemre.simplebank.dtos.request.account.CreateBankAccountRequest;
+import com.digitalemre.simplebank.dtos.request.transaction.CreateCreditRequest;
+import com.digitalemre.simplebank.dtos.request.transaction.CreateWithdrawalRequest;
 import com.digitalemre.simplebank.dtos.response.account.CreateBankAccountResponse;
+import com.digitalemre.simplebank.dtos.response.transaction.CreateCreditResponse;
+import com.digitalemre.simplebank.dtos.response.transaction.CreateWithdrawalResponse;
 import com.digitalemre.simplebank.entities.BankAccount;
 import com.digitalemre.simplebank.repositories.BankAccountRepository;
 import com.digitalemre.simplebank.repositories.TransactionRepository;
 import com.digitalemre.simplebank.services.BankAccountService;
 import com.digitalemre.simplebank.services.TransactionService;
 import com.digitalemre.simplebank.utilities.results.DataResult;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -23,8 +29,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
-class TransactionServiceImplTest {
+public class TransactionServiceImplTest {
 
     @Autowired
     private BankAccountService bankAccountService;
@@ -32,24 +37,65 @@ class TransactionServiceImplTest {
     @Autowired
     private BankAccountRepository bankAccountRepository;
 
+    @Autowired
+    private TransactionService transactionService;
+
+
+
+
     @Test
     public void createBankAccountTest() {
-        // Test için hazırlanan request
         CreateBankAccountRequest createBankAccountRequest = new CreateBankAccountRequest("John Doe");
 
-        // BankAccountService sınıfından createBankAccount metodunu çağırıyoruz
         DataResult<CreateBankAccountResponse> result = bankAccountService.createBankAccount(createBankAccountRequest);
 
-        // Sonuçları doğruluyoruz
         assertEquals(result.isSuccess(), true);
         assertEquals(result.getMessage(), "Bank account created successfully");
 
-        // Database'den yeni kaydın olup olmadığını kontrol ediyoruz
         BankAccount bankAccounts = bankAccountRepository.findByAccountNumber(result.getData().getAccountNumber());
         assertEquals(bankAccounts.getAccountNumber(), result.getData().getAccountNumber());
         assertEquals(bankAccounts.getOwner(), "John Doe");
     }
 
+
+    @Test
+    public void createBankAccountAndCreateCreditTest() {
+        CreateBankAccountRequest createBankAccountRequest = new CreateBankAccountRequest("John Doe");
+        DataResult<CreateBankAccountResponse> resultBankAccount = bankAccountService.createBankAccount(createBankAccountRequest);
+
+        String accountNumber =   resultBankAccount.getData().getAccountNumber();
+
+
+
+        CreateCreditRequest createCreditRequest = new CreateCreditRequest(1000.0);
+
+        DataResult<CreateCreditResponse> result = transactionService.creditAccount(accountNumber, createCreditRequest);
+
+        assertEquals(result.isSuccess(), true);
+        assertEquals(result.getMessage(), "Credit transaction created successfully");
+
+
+    }
+
+    @Test
+    public void createBankAccountAndCreateCreditAndCreateWithdrawalTest() {
+        CreateBankAccountRequest createBankAccountRequest = new CreateBankAccountRequest("John Doe Withdrawal");
+        DataResult<CreateBankAccountResponse> resultBankAccount = bankAccountService.createBankAccount(createBankAccountRequest);
+
+        String accountNumber =   resultBankAccount.getData().getAccountNumber();
+
+        CreateCreditRequest createCreditRequest = new CreateCreditRequest(1000.0);
+        DataResult<CreateCreditResponse> resultCredit = transactionService.creditAccount(accountNumber, createCreditRequest);
+
+        CreateWithdrawalRequest createWithdrawalRequest = new CreateWithdrawalRequest(500.0);
+
+        DataResult<CreateWithdrawalResponse> result = transactionService.withdrawAccount(accountNumber, createWithdrawalRequest);
+
+        assertEquals(resultCredit.isSuccess(), true);
+        assertEquals(result.isSuccess(), true);
+
+
+    }
 
 
 
